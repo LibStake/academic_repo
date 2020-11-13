500330ca044c90eeaa4c764d462ddafc03549c97
GipyoChoi <polling.choi@gmail.com>
2020-11-13 20:27:27 +0900

    Prep. PR3

---
+++ univ_classes/JAVA/PR3-13010986/src/Num1/JC13010986E1.java
@@ -0,0 +1,10 @@
+package Num1;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class JC13010986E1 {
+	public static void main(String [] args) {
+		System.out.println("13010986 √÷±‚«•");
+	}
+}
---
+++ univ_classes/JAVA/PR3-13010986/src/Num2/JC13010986E2.java
@@ -0,0 +1,7 @@
+package Num2;
+
+public class JC13010986E2 {
+	public static void main(String [] args) {
+		System.out.println("13010986 √÷±‚«•");
+	}
+}
