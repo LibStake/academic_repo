d9c182e11bfb3ad05e2bc5779101d158280bb3f5
GipyoChoi <polling.choi@gmail.com>
2020-12-07 04:18:29 +0900

    Update HW10

---
+++ univ_classes/JAVA/JAVA_HW_10/Test2-13010986/src/Num1/JT13010986_E1.java
@@ -0,0 +1,89 @@
+package Num1;
+
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class JT13010986_E1 extends JFrame {
+	
+	private JButton btn;
+	private JPanel up, bot;
+	private JLabel lbl;
+	
+	public static void main(String[] args) {
+		System.out.println("13010986최기표");
+		new JT13010986_E1();
+	}
+	
+	public JT13010986_E1() {
+		super("버튼에 Mouse 이벤트");
+		
+		this.setSize(400, 400);
+		
+		up = new JPanel();
+		up.setSize(400, 50);
+		up.setBackground(Color.yellow);
+		up.setLayout(new FlowLayout(FlowLayout.CENTER));
+		btn = new JButton("Mouse Event 테스트 버튼");
+		btn.setBackground(Color.yellow);
+		up.add(btn);
+		
+		bot = new JPanel();
+		bot.setSize(400, 350);
+		lbl = new JLabel("hello");
+		bot.add(lbl); lbl.setVisible(false);
+		
+		MouseEvent evt = new MouseEvent();
+		btn.addMouseListener(evt);
+		bot.addMouseListener(evt);
+		this.getContentPane().add(up);
+		this.getContentPane().add(bot);
+		this.setVisible(true);
+	}
+	
+	
+	private class MouseEvent implements MouseListener {
+
+		@Override
+		public void mouseClicked(java.awt.event.MouseEvent arg0) {
+			System.out.println("Click");
+			if (arg0.getComponent() == bot) {
+				System.out.println("Label");
+				lbl.setLocation(arg0.getPoint());
+				if (!lbl.isVisible()) lbl.setVisible(true);
+			}
+		}
+
+		@Override
+		public void mouseEntered(java.awt.event.MouseEvent arg0) {
+			System.out.println("Enter");
+			if (arg0.getComponent() == btn) {
+				btn.setBackground(Color.red);
+				up.setBackground(Color.red);
+			}
+		}
+
+		@Override
+		public void mouseExited(java.awt.event.MouseEvent arg0) {
+			System.out.println("Exit");
+			if (arg0.getComponent() == btn) {
+				btn.setBackground(Color.yellow);
+				up.setBackground(Color.yellow);
+			}
+		}
+
+		@Override
+		public void mousePressed(java.awt.event.MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(java.awt.event.MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+}
---
+++ univ_classes/JAVA/JAVA_HW_10/Test2-13010986/src/Num2/JT13010986_E2.java
@@ -0,0 +1,132 @@
+package Num2;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class JT13010986_E2 extends JFrame {
+	private LeftPanel left;
+	private RightPanel right;
+
+	public static void main(String[] args) {
+		System.out.println("13010986최기표");
+		new JT13010986_E2();
+	}
+	
+	public JT13010986_E2() {
+		super("13010986최기표");
+		
+		this.setSize(250,450);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Container con = getContentPane();
+		con.setLayout(new GridLayout(2, 1, 12, 12));
+		
+		left = new LeftPanel();
+		con.add(left);
+		Thread th1 = new Thread(left);
+		
+		
+		right = new RightPanel();
+		con.add(right);
+		Thread th2 = new Thread(right);
+		
+		this.setVisible(true);
+		th1.start();
+		th2.start();
+		
+	}
+
+	private class LeftPanel extends JPanel implements Runnable {
+		JLabel lblHelo;
+		Point pt;
+		
+		public LeftPanel() {
+			setSize(200, 200);
+			setBackground(Color.blue);
+			lblHelo = new JLabel("hello");
+			add(lblHelo);
+			addMouseListener(new MouseEvent());
+		}
+		
+		@Override
+		public void run() {
+			while (true) {
+				System.out.println("run!");
+				setLocation();
+			}
+		}
+		
+		synchronized void setLocation() {
+			System.out.println("setLoc!");
+			if (pt == null) {
+				try { System.out.println("wait!!"); wait(); } 
+				catch (InterruptedException e) { return; }
+			}
+			System.out.println("setted!");
+			lblHelo.setLocation(pt);
+			System.out.println("Done");
+		}
+		
+		synchronized void onClick(Point p) {
+			pt = p;
+			notify();
+		}
+		
+		private class MouseEvent implements MouseListener {
+			@Override
+			public void mouseClicked(java.awt.event.MouseEvent arg0) {
+				System.out.println("OnClick!");
+				onClick(arg0.getPoint());
+			}
+
+			@Override
+			public void mouseEntered(java.awt.event.MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseExited(java.awt.event.MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mousePressed(java.awt.event.MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseReleased(java.awt.event.MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		}
+	}
+	
+	private class RightPanel extends JPanel implements Runnable {
+		JLabel lblCnt;
+		int count = 0;
+		
+		public RightPanel() {
+			setSize(200, 200);
+			setBackground(Color.yellow);
+			lblCnt = new JLabel(Integer.toString(count));
+			lblCnt.setSize(30, 30);
+			add(lblCnt);
+		}
+		
+		@Override
+		public void run() {
+			while (true) {
+				try { Thread.sleep(1000); }
+				catch (InterruptedException e) { return; }
+				lblCnt.setText(Integer.toString(++count));
+			}
+		}
+	}
+	
+}
